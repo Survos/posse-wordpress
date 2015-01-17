d5e285829db69da0171e3aa7b10794b26ea5da2b
Peter Grochowski <piogrek@gmail.com>
2015-01-17 23:40:51 +0000

    removing actions for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,5 @@
 module.exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-composer');
-
     grunt.initConfig({
 
 
@@ -9,25 +7,12 @@ module.exports = function (grunt) {
             dev: {
                 options: {}
             }
-        },
-        copy: {
-            dev_config: {
-                src: 'config/wp-config.php.dev',
-                dest: 'src/wp-config.php',
-                options: {
-                    //process: function (content, srcpath) {
-                    //    return content.replace(/@GITVERSION@/g, grunt.config("meta.revision"));
-                    //}
-                }
-            },
-
-        },
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-git');
 
-    grunt.registerTask('dev', ['bower:install', 'copy:dev_config']);
-    grunt.registerTask('default', ['bower:install', 'copy:dev_config']);
+    grunt.registerTask('dev', []);
+    grunt.registerTask('default', ['cprod:dev_config']);
 
 };
