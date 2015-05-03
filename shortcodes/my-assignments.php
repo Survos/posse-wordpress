aa7c2d7c1007b7ef2ae108e18b63e6f525699a64
Tac Tacelosky <tacman@gmail.com>
2015-05-03 15:55:48 -0400

    add my-assignments

---
+++ shortcodes/my-assignments.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * return member data
+ * @param $atts
+ * @return string
+ */
+function my_posse_assignments($atts, $content = '')
+{
+    extract(
+        shortcode_atts(
+            [
+            ],
+            $atts
+        )
+    );
+
+    $user = Posse::getSymfonyUser();
+    if (!$mt = \Posse\SurveyBundle\Model\Type\MemberTypeQuery::create()->findOneByCode('personal')) // should be an attribute
+    {
+        return sprintf("Shortcode error: invalid memberType %s", $code);
+    }
+
+    $return = Posse::renderTemplate('PosseServiceBundle:Wordpress:shortcode.html.twig', [
+        'shortcode' => 'my-assignments',
+        'content'   => $content,
+        'data'      => [
+            'memberType' => $mt,
+            'member'     => ($mt && is_object($user)) ? $mt->memberQuery()->filterByUser($user)->findOne() : null, // missing $project!
+            'user'    => $user,
+            'wp_user' => get_currentuserinfo(),
+        ]
+    ]);
+
+    return $return;
+}
