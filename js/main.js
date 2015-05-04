92b82ea19599cf519435adf3b78d1a8e6dc61e69
Piotr Grochowski <piogrek@gmail.com>
2015-05-04 01:03:41 +0100

    fixing iframe heights (autosize)

--- js/main.js
+++ js/main.js
@@ -5,12 +5,18 @@
 jQuery(function ($) {
     console.log('posse plugin init (nothing loaded)');
 
+    var resizeIframe = function (obj) {
+        var height = obj.contentWindow.document.body.scrollHeight + 'px';
+        obj.style.height = height;
+        obj.parentNode.style.height = height;
+    }
+
     var handleIframeReload = function (iframe) {
         var $iframe = $(iframe);
         $iframe.load(function (e) {
             if (!$iframe.data('loaded')) {
                 $iframe.data('loaded', 1);
-                console.log('iframe loaded');
+                //console.log('iframe loaded');
             } else {
                 if ($iframe.data('reload-href')) {
                     window.location.assign($iframe.data('reload-href'));
@@ -23,6 +29,12 @@ jQuery(function ($) {
 
     $('[data-parent-reload]').each(function (a, b) {
         handleIframeReload(b);
+    });
+    $('.embed-autosize iframe').each(function (a, b) {
+        var iframe = $(b);
+        iframe.on('load', function () {
+            resizeIframe(b);
+        });
     });
 
     // $('.user-calendar').fullCalendar({});
